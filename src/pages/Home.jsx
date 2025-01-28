e3478a03e2de0d3c910b713306e5d8bdc77854fd
sagar gupta <sagarkshn8@gmail.com>
2025-01-28 19:43:27 +0530

    commit fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await fetch('https://api.shoopers-ecommerce.vercel.app/api/product/list'); // API to fetch products
+        const response = await fetch(backendUrl + '/api/product/list'); // API to fetch products
         const data = await response.json();
         
         if (Array.isArray(data.products)) {
