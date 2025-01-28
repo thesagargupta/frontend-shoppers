eb8e92ae63ee1a34c65ee22214d07e5da34dd892
sagar gupta <sagarkshn8@gmail.com>
2025-01-28 17:57:53 +0530

    commit fix

--- src/pages/Suggestion.jsx
+++ src/pages/Suggestion.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from 'react'; 
-import ProductCard from '../components/Navbar/ProductCard';
+import ProductCard from '../components/navbar/ProductCard';
 import { ShopContext } from '../context/ShopContext'; // Import the ShopContext
 import React from 'react';
 
